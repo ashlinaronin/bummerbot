f0acedee340d80ec766c1d86a6e9945cae57c09d
ashlinaronin <ashlinaronin@gmail.com>
2017-09-24 00:08:17 -0700

    Add a few new intents

--- services/intents.js
+++ services/intents.js
@@ -1,22 +1,27 @@
 const INTENTS = {
     PLAY_TECHNO: 'play_techno',
     WHO_ARE_YOU: 'who_are_you',
-    COIL: 'coil'
+    COIL: 'coil',
+    NATE: 'nate',
+    WHY_ARBYS: 'why_arbys'
 };
 
 const LINKS = {
     TECHNO_VID: 'https://www.youtube.com/watch?v=aUbMJvRj2uU',
-    DOH_GIF: 'https://giphy.com/gifs/thesimpsons-the-simpsons-3x24-xT5LMpBWU96Yivdfeo/embed'
 };
 
 function getResponseForIntent(intent) {
     switch (intent.value) {
         case (INTENTS.PLAY_TECHNO):
-            return `affirmative. playing techno. :techno: ${ LINKS.TECHNO_VID }`;
+            return `affirmative. playing :techno: ${ LINKS.TECHNO_VID }`;
         case (INTENTS.WHO_ARE_YOU):
             return `i'm bummerbot. :blackheart:`;
         case (INTENTS.COIL):
             return `:coil:`;
+        case (INTENTS.NATE):
+            return `:nate:`;
+        case (INTENTS.WHY_ARBYS):
+            return `I'm sorry if it bothers you. I like sandwiches and I want to share them with friends.`;
         default:
             return null;
     }
